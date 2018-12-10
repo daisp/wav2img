81fd01ba29ff2e010e5748ddd3315d1997f48ba7
Dais <daispaysagist@gmail.com>
2018-12-11 00:54:22 +0200

    Updates

---
+++ precache-manifest.34b57090e9acc19f735d427830233d88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wav2img/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d5ace914b32229614cae",
+    "url": "/wav2img/static/js/runtime~main.d5ace914.js"
+  },
+  {
+    "revision": "d11ab2f6c75c94b89569",
+    "url": "/wav2img/static/js/main.d11ab2f6.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/wav2img/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "d11ab2f6c75c94b89569",
+    "url": "/wav2img/static/css/main.c925f787.chunk.css"
+  },
+  {
+    "revision": "54c576fc8b44f3090e560ab61fbaf4cc",
+    "url": "/wav2img/index.html"
+  }
+];
